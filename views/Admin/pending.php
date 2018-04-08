632f57c7057923b509b430efd045f51f4e11d875
ak04 <albindoesgames@gmail.com>
2018-04-08 15:51:52 +0530

    Added comments for pending claims - currently not functional

--- views/Admin/pending.php
+++ views/Admin/pending.php
@@ -14,8 +14,8 @@
                 <th scope="col">POINTS</th>
                 <th scope="col">DOCUMENT</th>
                 <th scope="col">NOTES</th>
-                <th scope="col">DATE ADDED</th>
                 <th scope="col">USER</th>
+                <th scope="col">COMMENTS</th>
                 <th scope="col">ACTION</th>
             </tr>
         </thead>
@@ -31,9 +31,9 @@
                 <th>
                     <a class="btn btn-success" target="_blank" href="<?php echo ROOT_URL.$item['Document'];?>">View</a>
                 </th>
-                <th><?php echo $item['Notes']; ?></th>
-                <th><?php echo $item['AddDate']; ?></th>
+                <th><?php echo $item['Notes']; ?></th>                
                 <th><?php echo $item['User']; ?></th>
+                <th><input type="text" name="comments"></th>
                 <th class="btn-group-vertical">
                     <a class="btn btn-success btn-sm" href="<?php echo ROOTPATH; ?>?controller=admin&action=changeStatus&change=1&no=<?php echo $item['No']; ?>" style="margin-bottom: 2px;" >Accept</a>
                     <a class="btn btn-danger btn-sm" href="<?php echo ROOTPATH; ?>?controller=admin&action=changeStatus&change=2&no=<?php echo $item['No']; ?>">Reject</a>
