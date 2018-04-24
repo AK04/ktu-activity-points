9eb25168a853a7dcfd20e10418a44a42e487de47
Mikael <contact@mikaelgross.com>
2018-04-24 15:40:43 +0200

    Update index.php

--- views/Login/index.php
+++ views/Login/index.php
@@ -1,49 +1,56 @@
 <div class="jumbotron">
-
-    <div class="header text-center">
-      <h2><b>KTU ACIVITY POINTS <br>MEC</b></h2>
-    </div>    
-
-    <div class="panel panel-primary">
-
-        <div class="panel-body">
-
-            <form method="post" action="<?php $_SERVER['PHP_SELF']; ?>">
-
-                <div class="form-group">
-
-                    <label class="col-form-label">Username</label>
-                    <input type="text" name="username" class="form-control" />
-
-                </div>
-
-                <div class="form-group">
-
-                    <label class="col-form-label">Password</label>
-                    <input type="password" name="password" class="form-control" />
-
-                </div>
-
-                <div class="form-group">
-
-                    <label class="col-form-label" >Class:</label>
-
-                    <select class="form-control" name="class">
-
-                        <?php foreach($_SESSION['class'] as $item): ?>
-                        <option value="<?php echo $item['Class']; ?>"><?php echo $item['Class']; ?></option>
-                        <?php endforeach; ?>
-
-                    </select>
-
-                </div>
-
-                <input class="btn btn-primary" type="submit" name="submit" value="Login">
-
+    
+    <div class="card">
+        
+        <div class="header text-center">
+            
+            <h2><b>KTU ACIVITY POINTS <br>MEC</b></h2>
+            
+            <hr>
+            
+        </div>    
+        
+        <div class="panel panel-primary">
+            
+            <div class="panel-body">
+                
+                <form method="post" action="<?php $_SERVER['PHP_SELF']; ?>">
+                    
+                    <p class="lead">Please login with your identifiers:</p>
+                    
+                    <div class="form-group">
+                        
+                        <input type="text" name="username" class="form-control" placeholder="Username"/>
+                        
+                    </div>
+                                        
+                    <div class="form-group">
+                    
+                        <input type="password" name="password" class="form-control" placeholder="Password" />
+                    </div>
+                    
+                    <div class="form-group">
+                        
+                        <select class="form-control" name="class">
+                            
+                            <option value="" disabled selected>Select class..</option>
+                            
+                            <?php foreach($_SESSION['class'] as $item): ?>
+                            
+                            <option value="<?php echo $item['Class']; ?>"><?php echo $item['Class']; ?></option>
+                            
+                            <?php endforeach; ?>
+                            
+                        </select>
+                        
+                    </div>
+                    
+                <input class="btn btn-primary loginBtn" type="submit" name="submit" value="Login">
+                    
             </form>
-
+                
         </div>
-
+            
     </div>
-  
+        
 </div>
