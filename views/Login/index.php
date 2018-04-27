de634e1dc0419e251c1ccac1a2f58ea864dd0350
ak04 <albindoesgames@gmail.com>
2018-04-27 17:16:43 +0530

    Removed Jumbotron for Login page. Added Margin bottom for card

--- views/Login/index.php
+++ views/Login/index.php
@@ -1,55 +1,51 @@
-<div class="jumbotron">
+<div class="card">
     
-    <div class="card">
+    <div class="header text-center">
         
-        <div class="header text-center">
-            
-            <h2><b>KTU ACIVITY POINTS <br>MEC</b></h2>
-            
-            <hr>
-            
-        </div>    
+        <h2><b>KTU ACIVITY POINTS <br>MEC</b></h2>
         
-        <div class="panel panel-primary">
+        <hr>
+        
+    </div>    
+    
+    <div class="panel panel-primary">
+        
+        <div class="panel-body">
             
-            <div class="panel-body">
+            <form method="post" action="<?php $_SERVER['PHP_SELF']; ?>">
                 
-                <form method="post" action="<?php $_SERVER['PHP_SELF']; ?>">
-                    
-                    <p class="lead">Please login with your identifiers:</p>
-                    
-                    <div class="form-group">
-                        
-                        <input type="text" name="username" class="form-control" placeholder="Username"/>
-                        
-                    </div>
-                                        
-                    <div class="form-group">
-                    
-                        <input type="password" name="password" class="form-control" placeholder="Password" />
-                    </div>
-                    
-                    <div class="form-group">
-                        
-                        <select class="form-control" name="class">
-                            
-                            <option value="" disabled selected>Select class..</option>
-                            
-                            <?php foreach($_SESSION['class'] as $item): ?>
-                            
-                            <option value="<?php echo $item['Class']; ?>"><?php echo $item['Class']; ?></option>
-                            
-                            <?php endforeach; ?>
-                            
-                        </select>
-                        
-                    </div>
-                    
-                <input class="btn btn-primary loginBtn" type="submit" name="submit" value="Login">
-                    
-            </form>
+                <p class="lead">Please login with your identifiers:</p>
                 
-        </div>
+                <div class="form-group">
+                    
+                    <input type="text" name="username" class="form-control" placeholder="Username"/>
+                    
+                </div>
+                                    
+                <div class="form-group">
+                
+                    <input type="password" name="password" class="form-control" placeholder="Password" />
+                </div>
+                
+                <div class="form-group">
+                    
+                    <select class="form-control" name="class">
+                        
+                        <option value="" disabled selected>Select class..</option>
+                        
+                        <?php foreach($_SESSION['class'] as $item): ?>
+                        
+                        <option value="<?php echo $item['Class']; ?>"><?php echo $item['Class']; ?></option>
+                        
+                        <?php endforeach; ?>
+                        
+                    </select>
+                    
+                </div>
+                
+            <input class="btn btn-primary loginBtn" type="submit" name="submit" value="Login">
+                
+        </form>
             
     </div>
         
