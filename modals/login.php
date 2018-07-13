0ded3134fdb49b26b8f64bc8f6c2b81d48de6ea8
Albin Kottaram <albindoesgames@gmail.com>
2018-07-13 20:35:42 +0530

    Upgraded login logic for class teachers and students

--- modals/login.php
+++ modals/login.php
@@ -11,34 +11,22 @@ class LoginModel extends Model{
 
 		$_SESSION['class'] = $rows;
 
+
 		if($post['submit']) {
 
-			if($post['username'] == 'admin') {
+			$this->query('SELECT * FROM users WHERE username = :username AND password = :password;');
+			$this->bind(':username', $post['username']);
+			$this->bind(':password', $post['password']);
+			
+			$row = $this->single();
 
-				$this->query('SELECT * FROM users WHERE username = :username AND password = :password AND Class = :class;');
-				$this->bind(':username', $post['username']);
-				$this->bind(':password', $post['password']);
-				$this->bind(':class', $post['class']);
-				
-				$row = $this->single();
-
-			} 
-
-			else {
-
-				$this->query('SELECT * FROM users WHERE username = :username AND password = :password;');
-				$this->bind(':username', $post['username']);
-				$this->bind(':password', $post['password']);
-				
-				$row = $this->single();
-
-			}
 
 			if($row){
+
 				$_SESSION['is_logged_in'] = true;
 				$_SESSION['user'] = $row['username'];
 
-				if ($_SESSION['user'] == 'admin') {
+				if ($row['Admin'] == 1) {
 					unset($_SESSION['class']);
 					$_SESSION['class'] = $post['class'];					
 					header('Location: '.ROOT_URL.'?controller=admin');
