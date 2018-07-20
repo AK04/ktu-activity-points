ad25d202873f699c733c7aa5af8a93a879ee9458
Albin Kottaram <albindoesgames@gmail.com>
2018-07-20 13:09:57 +0530

    Added more year options

--- views/Admin/pending.php
+++ views/Admin/pending.php
@@ -15,8 +15,9 @@
                 <th scope="col">ACTIVITY</th>
                 <th scope="col">POINTS</th>
                 <th scope="col">DOCUMENT</th>
+                <th scope="col">YEAR</th>
                 <th scope="col">NOTES</th>
-                <th scope="col">USER</th>
+                <th scope="col">STUDENT</th>
                 <th scope="col">COMMENTS</th>
                 <th scope="col">ACTION</th>
             </tr>
@@ -30,10 +31,10 @@
                 <th><?php echo $i ?></th>
                 <th><?php echo $item['Activity']; ?></th>
                 <th><?php echo $item['Points']; ?></th>
-
                 <th>
                     <a class="btn btn-success" target="_blank" href="<?php echo ROOT_URL.$item['Document'];?>">View</a>
                 </th>
+                <th><?php echo $item['Year']; ?></th>                
                 <th><?php echo $item['Notes']; ?></th>                
                 <th><?php echo $item['User']; ?></th>
 
